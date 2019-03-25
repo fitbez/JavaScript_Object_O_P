44374031a88d56906c55c60a8d7e8cde96a95482
fitsum belayneh <fitsu_alt@yahoo.com>
2019-03-25 14:11:39 -0400

    Object

---
+++ 1_basics_literals.js
@@ -0,0 +1,10 @@
+// const s1 = "Hello";
+// console.log(typeof s1); /* String */
+
+// const s2 = new String("Hello");
+// console.log(typeof s2); /* Object */
+
+// console.log(window); /* window is an absolute parent object */
+// alert(1);
+
+// console.log(navigator.appVersion);
