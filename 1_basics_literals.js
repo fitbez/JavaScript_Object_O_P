f75446a21cd10fc621948be7b09e53572265fdc5
fitsum belayneh <fitsu_alt@yahoo.com>
2019-03-25 14:42:40 -0400

    Object literal

--- 1_basics_literals.js
+++ 1_basics_literals.js
@@ -8,3 +8,16 @@
 // alert(1);
 
 // console.log(navigator.appVersion);
+
+/* Object Literals */
+
+const book1 = {
+  title: "Book One",
+  author: "John Doe",
+  year: "2013",
+  getSummary: function() {
+    return `${this.title} was written by ${this.author} in ${this.year}`;
+  }
+};
+
+console.log(book1.getSummary());
