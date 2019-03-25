3dfa68e68ae9442d354f588921e759dbbd350c83
fitsum belayneh <fitsu_alt@yahoo.com>
2019-03-25 15:53:46 -0400

    constructor

--- _constructor.js
+++ _constructor.js
@@ -3,6 +3,10 @@ function Book(title, author, year) {
   this.title = title;
   this.author = author;
   this.year = year;
+
+  this.getSummary = function() {
+    return `${this.title} was written by ${this.author} in ${this.year}`;
+  };
 }
 
 // Instantiate an Object
@@ -11,3 +15,5 @@ const book2 = new Book("Book2", "Jane Doe", "2016");
 
 console.log(book1.title);
 console.log(book2.year);
+
+console.log(book2.getSummary());
